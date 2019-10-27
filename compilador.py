8f0caec9e0677b60d4ad4269c0d65303b215228b
Amon <amonrhaniery27@gmail.com>
2019-10-27 16:03:05 -0300

    final version

--- compilador.py
+++ compilador.py
@@ -3,8 +3,6 @@ import analisadorLexico as lxc
 import analisadorSintatico as stt
 
 lxc.arq = open('codigo2.ssl', 'r')
-#lxc.arq = open('NoErrors.ssl', 'r')
-#lxc.analisarLexicamente(arq)
 stt.analisarSintaticamente()
 
 lxc.arq.close()
